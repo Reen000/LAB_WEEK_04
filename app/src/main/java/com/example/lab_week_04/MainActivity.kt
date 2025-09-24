ea410d3c2954cf6c879eec611b494ff13073bb6c
Reen000 <alexandriamaureen8@@gmail.com>
2025-09-24 17:38:04 +0700

    Commit No. 2 – Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -5,35 +5,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-//class MainActivity : AppCompatActivity() {
-//    private lateinit var appBarConfiguration: AppBarConfiguration
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        setSupportActionBar(findViewById(R.id.toolbar))
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-//                    as NavHostFragment
-//        val navController = navHostFragment.navController
-//        //Creating top level destinations
-//        //and adding them to the draw
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.listFragment, R.id.favoritesFragment
-//            ), findViewById(R.id.drawer_layout)
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        findViewById<NavigationView>(R.id.nav_view)
-//            ?.setupWithNavController(navController)
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        super.onSupportNavigateUp()
-//        return TODO("Provide the return value")
-//    }
-//}
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,11 +22,16 @@ class MainActivity : AppCompatActivity() {
         //and adding them to the draw
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.listFragment, R.id.favoritesFragment
+                R.id.listFragment, R.id.favoritesFragment, R.id.cafeFragment
             ), findViewById(R.id.drawer_layout)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
+
         findViewById<NavigationView>(R.id.nav_view)
+            ?.setupWithNavController(navController)
+
+        // Added this part only
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
             ?.setupWithNavController(navController)
     }
     override fun onSupportNavigateUp(): Boolean {
@@ -61,5 +40,6 @@ class MainActivity : AppCompatActivity() {
                 super.onSupportNavigateUp()
     }
 }
+
 
 
